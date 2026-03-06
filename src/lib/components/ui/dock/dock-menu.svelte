06d34759ac5e85f8f5ab06dc2f08e79505d2ab32
Sankkit <SankkitWilson@hotmail.com>
2026-03-07 03:43:14 +1030

    Refactor menu items to shared type

--- src/lib/components/ui/dock/dock-menu.svelte
+++ src/lib/components/ui/dock/dock-menu.svelte
@@ -2,62 +2,10 @@
 	import { Motion, useMotionValue } from 'svelte-motion';
 	import { cn } from '$lib/utils';
 	import DockItem from './dock-item.svelte';
-	import { User, House, MonitorIcon, LibraryBig, Camera } from 'lucide-svelte';
-	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
-
-	type DockItem = {
-		id: string;
-		label: string;
-		icon?: {
-			component: any;
-			props?: Record<string, any>;
-		};
-		href: string;
-	};
-
-	const icons: Record<string, DockItem['icon']> = {
-		homeIcon: {
-			component: House,
-			props: {
-				size: 32
-			}
-		},
-		aboutIcon: {
-			component: User,
-			props: {
-				size: 32
-			}
-		},
-		projectsIcon: {
-			component: MonitorIcon,
-			props: {
-				size: 32
-			}
-		},
-		skillsIcon: {
-			component: LibraryBig,
-			props: {
-				size: 32
-			}
-		},
-		cameraIcon: {
-			component: Camera,
-			props: {
-				size: 32
-			}
-		}
-	};
-
+	import { menuItems } from '$lib/utils/menu.utils';
 	export let side: 'top' | 'bottom' = 'bottom';
 	export let className: string;
 	export { className as class };
-	export const items: DockItem[] = [
-		{ id: '1', label: 'Home', icon: icons['homeIcon'], href: '/' },
-		{ id: '2', label: 'About', icon: icons['aboutIcon'], href: '/test' },
-		{ id: '3', label: 'Projects', icon: icons['projectsIcon'], href: '/foo' },
-		{ id: '4', label: 'Skills', icon: icons['skillsIcon'], href: '/' },
-		{ id: '5', label: 'Photos', icon: icons['cameraIcon'], href: '/' }
-	];
 
 	const mouseX = useMotionValue(Infinity);
 	const containerX = useMotionValue(0);
@@ -81,7 +29,7 @@
 				}
 			}}
 		>
-			{#each items as dockItem}
+			{#each menuItems as dockItem}
 				<DockItem {containerX} {mouseX} href={dockItem.href}>
 					{#if dockItem?.icon}
 						<svelte:component this={dockItem.icon.component} {...dockItem.icon.props} />
