cba00ca2ab2975a52e810a2fbc3db411c9e2caed
Sankkit <SankkitWilson@hotmail.com>
2026-03-06 22:49:06 +1030

    Add path to config

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import adapter from "@sveltejs/adapter-static";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
+        paths: "sankeetos-portfolio",
         adapter: adapter({
             pages: "build",
             assets: "build",
