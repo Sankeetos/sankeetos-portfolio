d956dd0aa353a5a624e2d5456b1b6573cf985965
Sankkit <SankkitWilson@hotmail.com>
2026-03-06 23:23:34 +1030

    Remove paths.assets

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import adapter from "@sveltejs/adapter-static";
+import process from "node:process";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
         paths: {
-            base: "/sankeetos-portfolio",
-            assets: "/sankeetos-portfolio",
+            base: process.argv.includes("dev") ? "" : "/sankeetos-portfolio",
         },
         adapter: adapter({
             pages: "build",
