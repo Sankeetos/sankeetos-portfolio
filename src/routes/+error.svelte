1dd1448bafd6cc63a7d1c9177e996e5cd7153887
Sankkit <SankkitWilson@hotmail.com>
2026-03-07 16:05:48 +1030

    Fix error page covering nav buttons

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,7 +3,7 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
-<div class="fixed top-0 left-0 flex h-full w-full content-center items-center justify-center">
+<div class="fixed flex h-full w-full content-center items-center justify-center">
 	<p class="font-pinyon text-6xl font-semibold">Error</p>
 	<Button variant="link" class="justify-center text-6xl" href="/">
 		{page.status}
