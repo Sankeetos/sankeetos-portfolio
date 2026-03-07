afb83d06592b056b500cf9abae127729ce1160b8
Sankkit <SankkitWilson@hotmail.com>
2026-03-07 16:03:30 +1030

    Create fallback error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<div class="fixed top-0 left-0 flex h-full w-full content-center items-center justify-center">
+	<p class="font-pinyon text-6xl font-semibold">Error</p>
+	<Button variant="link" class="justify-center text-6xl" href="/">
+		{page.status}
+	</Button>
+</div>
