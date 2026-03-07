5bdd4cbeda5e545b4db7a96aa9cf5bf648dd9164
Sankkit <SankkitWilson@hotmail.com>
2026-03-07 14:55:48 +1030

    Update hero layout

--- src/lib/components/ui/hero/hero.svelte
+++ src/lib/components/ui/hero/hero.svelte
@@ -1,12 +1,12 @@
-<div class="flex flex-col overflow-hidden p-8 pb-0">
-	<h1 class="text-2xl leading-tight font-semibold text-purple-800 md:text-9xl">
-		Sankkit Wilson-Hartono
-	</h1>
+<div class="flex flex-col p-4 pb-15 text-purple-600">
+	<h1 class="z-10 text-4xl font-semibold md:text-7xl lg:text-9xl">Sankkit Wilson-Hartono</h1>
 
-	<h2 class="font-pinyon text-xl md:text-8xl">Software Engineer</h2>
+	<h2 class="z-10 -mt-2 font-pinyon text-4xl md:-mt-2 md:ml-4 md:text-7xl lg:text-9xl">
+		Software Engineer
+	</h2>
 </div>
 
-<div class="m-0 w-lg p-8 pb-0">
+<div class="p-8">
 	<div class="mt-4 flex flex-col gap-2 text-sm text-gray-600 md:text-2xl">
 		<p class="w-auto text-ellipsis md:text-clip">
 			Welcome, this is my portfolio. It is currently a work in progress.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,3 @@
 <Hero />
 
 <SankkitModelDancing />
-
-<p class="h-full w-full bg-red-500"></p>
-
-<p class="h-full w-full bg-red-500"></p>
-
-<h1 id="About" class="text-3xl">About</h1>
