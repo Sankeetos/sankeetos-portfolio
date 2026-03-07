0c9b8904846e770e9198b59e41fe3bb6b3ab136f
Sankkit <SankkitWilson@hotmail.com>
2026-03-07 18:10:26 +1030

    Create small text stroke

--- src/lib/components/ui/hero/hero.svelte
+++ src/lib/components/ui/hero/hero.svelte
@@ -1,10 +1,12 @@
 <div class="flex flex-col p-4 pb-15 text-purple-600">
-	<h1 class="self-start text-3xl font-semibold text-nowrap md:text-6xl lg:text-7xl xl:text-8xl">
+	<h1
+		class="self-start text-3xl font-semibold text-nowrap select-none md:text-6xl lg:text-7xl xl:text-8xl"
+	>
 		Sankkit Wilson-Hartono
 	</h1>
 
 	<h2
-		class="text-stroke -mt-2 ml-[40vw] font-pinyon text-3xl font-bold text-nowrap md:-mt-6 md:ml-[40vw] md:text-6xl lg:-mt-8 lg:ml-[40vw] lg:text-7xl xl:-mt-9 xl:ml-[30vw] xl:text-8xl"
+		class="text-stroke-small md:text-stroke -mt-4 ml-[30vw] font-pinyon text-3xl font-bold text-nowrap select-none md:-mt-6 md:ml-[40vw] md:text-6xl lg:-mt-8 lg:ml-[40vw] lg:text-7xl xl:-mt-9 xl:ml-[35vw] xl:text-8xl"
 	>
 		Software Engineer
 	</h2>
