5d84c91cc0457bb0d3ee268f1885b4d3095d8bae
Sankkit <SankkitWilson@hotmail.com>
2026-03-09 17:38:53 +1030

    Heading style updates

--- src/lib/components/ui/hero/hero.svelte
+++ src/lib/components/ui/hero/hero.svelte
@@ -1,13 +1,13 @@
 <div class="flex flex-col p-4 pb-15">
 	<h1
-		class="self-start text-4xl font-semibold text-nowrap select-none md:text-6xl lg:text-7xl xl:text-8xl"
+		class="self-start text-3xl font-semibold text-nowrap select-none md:text-6xl lg:text-7xl xl:text-8xl"
 	>
 		Sankkit Wilson-Hartono
 	</h1>
 
 	<h2
 		class="text-stroke-small md:text-stroke -mt-4 ml-[30vw] font-pinyon text-4xl font-bold text-nowrap
-		text-purple-600 select-none md:-mt-6 md:ml-[40vw] md:text-6xl lg:-mt-8 lg:ml-[40vw] lg:text-7xl xl:-mt-9 xl:ml-[35vw] xl:text-8xl"
+		text-purple-600 select-none md:-mt-6 md:ml-[40vw] md:text-6xl lg:-mt-8 lg:ml-[40vw] lg:text-7xl xl:-mt-9 xl:ml-[21vw] xl:text-8xl"
 	>
 		Software Engineer
 	</h2>
