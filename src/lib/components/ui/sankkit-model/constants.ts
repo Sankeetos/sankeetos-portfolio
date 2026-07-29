255368dcc56d681c7a21fbc704c28a2ca673d66d
sankkitwilson <wilsy103@mymail.unisa.edu.au>
2026-07-29 21:45:03 +0930

    Fix accidental constant removal

--- src/lib/components/ui/sankkit-model/constants.ts
+++ src/lib/components/ui/sankkit-model/constants.ts
@@ -5,6 +5,7 @@ export const FOV = 75;
 export const LARGE_SCALE = 1.3;
 export const WIDTH = window.innerWidth;
 export const HEIGHT = window.innerHeight;
+export const DEFAULT_MOBILE_BREAKPOINT = 768;
 // Width of the element divided by the height, to not get a squished image
 export const ASPECT_RATIO = WIDTH / HEIGHT;
 export const DEFAULT_POSITION = new Vector3(0, 1.5, 0);
