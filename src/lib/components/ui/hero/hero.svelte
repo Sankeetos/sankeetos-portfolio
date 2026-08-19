b34c0532becf01cdb391a631bea044d070a33252
Sankkit <105831498+Sankeetos@users.noreply.github.com>
2026-08-19 19:48:02 +0930

    dev to master (#21)
    * 3d model size (#12)
    
    * Clean-up constants
    
    * Clean up imports
    
    * Simplify resizing and rendering
    
    * Fix accidental constant removal
    
    * Quick fix (#15)
    
    * 3d model size (#12) (#13)
    
    * Clean-up constants
    
    * Clean up imports
    
    * Simplify resizing and rendering
    
    * Fix accidental constant removal
    
    * Update hero (#17)
    
    * 3d model size (#12) (#13)
    
    * Clean-up constants
    
    * Clean up imports
    
    * Simplify resizing and rendering
    
    * Fix accidental constant removal
    
    * Quick fix (#15)
    
    * 3d model size (#12) (#13)
    
    * Clean-up constants
    
    * Clean up imports
    
    * Simplify resizing and rendering
    
    * Fix accidental constant removal
    
    * Add buttons to hero
    
    * Fix flex spelling
    
    * Random greek character
    
    * simplify ui, remove all unneeded shadcn components (#19)
    
    * Fix build (#14)
    
    * 3d model size (#12)
    
    * Clean-up constants
    
    * Clean up imports
    
    * Simplify resizing and rendering
    
    * Fix accidental constant removal
    
    * Quick fix (#15)
    
    * 3d model size (#12) (#13)
    
    * Clean-up constants
    
    * Clean up imports
    
    * Simplify resizing and rendering
    
    * Fix accidental constant removal
    
    * Quick fix (#15)
    
    * 3d model size (#12) (#13)
    
    * Clean-up constants
    
    * Clean up imports
    
    * Simplify resizing and rendering
    
    * Fix accidental constant removal
    
    * Quick fix (#15)
    
    * 3d model size (#12) (#13)
    
    * Clean-up constants
    
    * Clean up imports
    
    * Simplify resizing and rendering
    
    * Fix accidental constant removal
    
    * simplify ui, remove all unneeded shadcn components (#19)
    
    * Fix build (#14)
    
    * 3d model size (#12)
    
    * Clean-up constants
    
    * Clean up imports
    
    * Simplify resizing and rendering
    
    * Fix accidental constant removal
    
    * Quick fix (#15)
    
    * 3d model size (#12) (#13)
    
    * Clean-up constants
    
    * Clean up imports
    
    * Simplify resizing and rendering
    
    * Fix accidental constant removal
    
    * Quick fix (#15)
    
    * 3d model size (#12) (#13)
    
    * Clean-up constants
    
    * Clean up imports
    
    * Simplify resizing and rendering
    
    * Fix accidental constant removal

--- src/lib/components/ui/hero/hero.svelte
+++ src/lib/components/ui/hero/hero.svelte
@@ -1,29 +1,14 @@
-<script>
-	import { User, Trophy } from 'lucide-svelte';
-	import Button from '../button/button.svelte';
-</script>
-
-<div class="flex flex-col p-4 pb-15">
+<div class="my-10 flex flex-col p-4 pb-15">
 	<h1
-		class="self-start text-3xl font-semibold text-nowrap select-none md:text-6xl lg:text-7xl xl:text-8xl"
+		class="self-center text-3xl font-semibold text-nowrap select-none md:text-6xl lg:text-7xl xl:text-8xl"
 	>
 		Sankkit Wilson-Hartono
 	</h1>
 
 	<h2
-		class="text-stroke-small md:text-stroke -mt-4 ml-[30vw] font-pinyon text-4xl font-bold text-nowrap
-		text-purple-600 select-none md:-mt-6 md:ml-[40vw] md:text-6xl lg:-mt-8 lg:ml-[40vw] lg:text-7xl xl:-mt-9 xl:ml-[21vw] xl:text-8xl"
+		class="text-stroke-small md:text-stroke -mt-4 self-center font-pinyon text-4xl font-bold text-nowrap
+		text-purple-600 select-none md:-mt-6 md:text-6xl lg:-mt-8 lg:text-7xl xl:-mt-9 xl:text-8xl"
 	>
 		Software Engineer
 	</h2>
-</div>
-
-<div class="justify-items-center p-2 md:w-150 md:p-8">
-	<div class="flex flex-col gap-2 text-sm md:text-xl lg:text-2xl">
-		<p class="w-auto text-ellipsis md:text-clip">Hi, my portfolio is current a WIP.</p>
-	</div>
-	<div>
-		<Button variant="ghost"><User /> About</Button>
-		<Button variant="ghost"><Trophy /> Experience</Button>
-	</div>
 </div>
