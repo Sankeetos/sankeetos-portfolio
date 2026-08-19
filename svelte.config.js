7e168ead34f29ff63b8d12483718afb34d007261
Sankeetos <SankkitWilson@hotmail.com>
2026-08-19 19:37:47 +0930

    simplify ui, remove all unneeded shadcn components

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,19 @@
-import adapter from "@sveltejs/adapter-static";
-import process from "node:process";
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        paths: {
-            assets: "https://sankkitwilson.dev",
-        },
-        adapter: adapter({
-            pages: "build",
-            assets: "build",
-            fallback: "404.html",
-            precompress: false,
-            strict: true,
-        }),
-    },
+	kit: {
+		paths: {
+			assets: 'https://sankkitwilson.dev'
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		})
+	}
 };
 
 export default config;
